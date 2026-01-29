4db4171e68aa9bd0c4d34b58e37a8d6718eaf34d
Jhonata-eng <jhonatasousaribeiro@gmail.com>
2026-01-29 17:04:19 -0300

    Update ForgotPasswordContainer.tsx

--- resources/scripts/components/auth/ForgotPasswordContainer.tsx
+++ resources/scripts/components/auth/ForgotPasswordContainer.tsx
@@ -38,7 +38,7 @@ export default () => {
                 console.error(error);
 
                 setSubmitting(false);
-                addFlash({ type: 'error', title: 'Error', message: httpErrorToHuman(error) });
+                addFlash({ type: 'error', title: 'Erro', message: httpErrorToHuman(error) });
             });
 
             return;
@@ -47,11 +47,11 @@ export default () => {
         requestPasswordResetEmail(email, token)
             .then((response) => {
                 resetForm();
-                addFlash({ type: 'success', title: 'Success', message: response });
+                addFlash({ type: 'success', title: 'Successo', message: response });
             })
             .catch((error) => {
                 console.error(error);
-                addFlash({ type: 'error', title: 'Error', message: httpErrorToHuman(error) });
+                addFlash({ type: 'error', title: 'Erro', message: httpErrorToHuman(error) });
             })
             .then(() => {
                 setToken('');
@@ -67,24 +67,24 @@ export default () => {
             initialValues={{ email: '' }}
             validationSchema={object().shape({
                 email: string()
-                    .email('A valid email address must be provided to continue.')
-                    .required('A valid email address must be provided to continue.'),
+                    .email('É necessário informar um e-mail válido para continuar.')
+                    .required('É necessário informar um e-mail válido para continuar.'),
             })}
         >
             {({ isSubmitting, setSubmitting, submitForm }) => (
-                <LoginFormContainer title={'Request Password Reset'} css={tw`w-full flex`}>
+                <LoginFormContainer title={'Solicitar reset de senha'} css={tw`w-full flex`}>
                     <Field
                         light
-                        label={'Email'}
+                        label={'E-mail'}
                         description={
-                            'Enter your account email address to receive instructions on resetting your password.'
+                            'Digite seu e-mail cadastrado para receber instruções de como resetar sua senha.'
                         }
                         name={'email'}
                         type={'email'}
                     />
                     <div css={tw`mt-6`}>
                         <Button type={'submit'} size={'xlarge'} disabled={isSubmitting} isLoading={isSubmitting}>
-                            Send Email
+                            Enviar e-mail
                         </Button>
                     </div>
                     {recaptchaEnabled && (
@@ -107,7 +107,7 @@ export default () => {
                             to={'/auth/login'}
                             css={tw`text-xs text-neutral-500 tracking-wide uppercase no-underline hover:text-neutral-700`}
                         >
-                            Return to Login
+                            Voltar ao Login
                         </Link>
                     </div>
                 </LoginFormContainer>
