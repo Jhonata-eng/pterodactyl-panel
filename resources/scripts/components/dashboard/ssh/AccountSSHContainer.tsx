ad5ebcbd0af007c0d05b0de4cc7c61584308b2fc
Jhonata-eng <jhonatasousaribeiro@gmail.com>
2026-02-01 01:16:52 +0000

    Correção tradução ssh

--- resources/scripts/components/dashboard/ssh/AccountSSHContainer.tsx
+++ resources/scripts/components/dashboard/ssh/AccountSSHContainer.tsx
@@ -48,7 +48,7 @@ export default () => {
                                     <p css={tw`text-sm break-words font-medium`}>{key.name}</p>
                                     <p css={tw`text-xs mt-1 font-mono truncate`}>SHA256:{key.fingerprint}</p>
                                     <p css={tw`text-xs mt-1 text-neutral-300 uppercase`}>
-                                        Added on:&nbsp;
+                                        Adicionado em:&nbsp;
                                         {format(key.createdAt, 'MMM do, yyyy HH:mm')}
                                     </p>
                                 </div>
